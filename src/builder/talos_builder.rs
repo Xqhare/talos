ae92fe76d5df60bec306d1fac2fb336505fc7d49
Xqhare <xqhare@web.de>
2026-01-25 00:44:34 +0100

    add(TalosBuilder): support max_poll_input_buffer

--- src/builder/talos_builder.rs
+++ src/builder/talos_builder.rs
@@ -1,15 +1,19 @@
-use crate::{error::TalosResult, render::{Canvas, Codex}, terminal::term_io::TerminalIO, Talos};
+use std::u16;
+
+use crate::{error::TalosResult, render::{CCell, Canvas, Codex}, terminal::term_io::TerminalIO, Talos};
 
 pub struct TalosBuilder {
     hide_cursor: bool,
     alternate_screen: bool,
+    max_poll_input_buffer: u16,
 }
 
 impl Default for TalosBuilder {
     fn default() -> Self {
         Self { 
             hide_cursor: true, 
-            alternate_screen: true 
+            alternate_screen: true,
+            max_poll_input_buffer: 4096,
         }
     }
 }
@@ -20,19 +24,37 @@ impl TalosBuilder {
         self
     }
 
+    pub fn with_alternate_screen(mut self) -> Self {
+        self.alternate_screen = true;
+        self
+    }
+
+    /// The default supports 4kb of input per frame
+    pub fn with_max_poll_input_buffer(mut self, max_poll_input_buffer: u16) -> Self {
+        self.max_poll_input_buffer = max_poll_input_buffer;
+        self
+    }
+
     pub fn build(self) -> TalosResult<Talos> {
         // Initialize TerminalIO based on these settings
         let terminal = TerminalIO::new(self.hide_cursor, self.alternate_screen)?;
         let size = terminal.size()?;
         let codex = Codex::new();
+
+        let buffer_size = (size.0 as usize) * (size.1 as usize);
+        let previous_buffer = vec![CCell::default(); buffer_size];
+        // 10 bytes per cell may seem overkill, with a lot of styling bytes this may not
+        //    even be enough!
+        let output_buffer = Vec::with_capacity(buffer_size * 10);
         
         Ok(Talos {
             terminal,
             canvas: Canvas::new(size.1, size.0),
             size,
             codex,
-            current_buffer: None,
-            previous_buffer: None
+            previous_buffer,
+            output_buffer,
+            max_poll_input_buffer: self.max_poll_input_buffer
         })
     }
 }
