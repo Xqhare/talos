eac9a0d15212f171d5cf6a220b9d744795b08bcc
Xqhare <xqhare@web.de>
2026-01-24 02:51:51 +0100

    feat(lib): make present and begin_frame with current & previous buffers, and present work with one dimensional canvas array

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::io::Write;
 
 use builder::TalosBuilder;
-use constants::ansi::TO_TOP_LEFT;
+use constants::ansi::{CLEAR_ALL, TO_TOP_LEFT};
 use error::TalosResult;
 use render::Canvas;
 use terminal::term_io::TerminalIO;
@@ -22,6 +22,8 @@ pub struct Talos {
     // Terminal Size
     /// Width, Height
     size: (Width, Height),
+    current_buffer: Option<Vec<u8>>,
+    previous_buffer: Option<Vec<u8>>,
 }
 
 impl Talos {
@@ -30,18 +32,40 @@ impl Talos {
     }
 
     pub fn begin_frame(&mut self) {
+        if self.current_buffer.is_some() {
+            self.previous_buffer = self.current_buffer.take();
+        }
         self.canvas.clear();
     }
 
     pub fn present(&mut self) -> TalosResult<()> {
-        write!(self.terminal, "{}", TO_TOP_LEFT)?;
-        for row in self.canvas.buffer.iter() {
-            for cell in row.iter() {
-                write!(self.terminal, "{}", cell.char)?;
+        // TODO: Intercept `SIGWINCH` and update terminal size
+        
+        let mut buffer = Vec::new();
+        write!(buffer, "{}", CLEAR_ALL)?;
+        write!(buffer, "{}", TO_TOP_LEFT)?;
+
+        for y in 0..self.size.1 {
+            for x in 0..self.size.0 {
+                let ccell = self.canvas.get_ccell(x, y);
+                write!(buffer, "{}", ccell.to_string())?;
             }
-            write!(self.terminal, "\r\n")?;
+            write!(buffer, "\n")?;
         }
-        self.terminal.stdout().flush()?;
+
+        // if prev buffer is some, take the diff with current buffer and write only changed
+        // cells
+        if let Some(prev_buffer) = self.previous_buffer.as_ref() {
+            let mut diff = Vec::new();
+            for i in 0..buffer.len() {
+                if prev_buffer[i] != buffer[i] {
+                    diff.push(buffer[i]);
+                }
+            }
+            self.terminal.stdout().write(&diff)?;
+        }
+
+        self.terminal.stdout().write(&buffer)?;
         Ok(())
     }
 }
