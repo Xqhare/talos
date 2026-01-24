13ceaaaa70750914531f506ae97c4be00d3c86fe
Xqhare <xqhare@web.de>
2026-01-24 02:52:12 +0100

    feat(canvas): make buffer into one dimensional array

--- src/render/canvas.rs
+++ src/render/canvas.rs
@@ -1,37 +1,39 @@
-use crate::constants::ansi::{CLEAR_ALL, TO_TOP_LEFT};
 
-use super::{CCell, Style};
+use super::CCell;
 
 pub struct Canvas {
-    pub buffer: Vec<Vec<CCell>>,
+    pub buffer: Vec<CCell>,
     width: u16,
     height: u16,
 }
 
 impl Canvas {
     pub fn new(width: u16, height: u16) -> Self {
-        let w = width as usize;
-        let h = height as usize;
+        let buffer = make_default_buffer(width, height);
         Self {
-            buffer: vec![
-                vec![
-                    CCell { 
-                        char: ' ', 
-                        style: Style::default() 
-                    }; 
-                    w]; 
-                h],
+            buffer,
             width,
             height,
         }
     }
     
     pub fn clear(&mut self) {
-        for row in self.buffer.iter_mut() {
-            for cell in row.iter_mut() {
-                *cell = CCell::default();
-            }
-        }
+        self.buffer = make_default_buffer(self.width, self.height);
+    }
+
+    pub fn get_ccell(&self, x: u16, y: u16) -> CCell {
+        self.buffer[(x + y * self.width) as usize]
+    }
+
+    pub fn get_mut_ccell(&mut self, x: u16, y: u16) -> &mut CCell {
+        &mut self.buffer[(x + y * self.width) as usize]
+    }
+
+    pub fn set_ccell(&mut self, x: u16, y: u16, cell: CCell) {
+        self.buffer[(x + y * self.width) as usize] = cell;
     }
 }
 
+fn make_default_buffer(width: u16, height: u16) -> Vec<CCell> {
+    vec![CCell::default(); width as usize * height as usize]
+}
