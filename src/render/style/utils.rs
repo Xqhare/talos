767fe5f149def2bd064b7272896d21d804959651
Xqhare <xqhare@web.de>
2026-01-26 14:45:40 +0100

    feat(render/style): heavy optimisations

--- src/render/style/utils.rs
+++ src/render/style/utils.rs
@@ -2,50 +2,60 @@ use crate::render::{colour::{Extended, BG_PRE_DIGIT_BRIGHT, BG_PRE_DIGIT_NORMAL,
 
 
 
-pub fn handle_fg(colour: Colour) -> String {
-    handle_colour(colour, true)
+pub fn handle_fg(colour: Colour, output_buffer: &mut Vec<u8>) {
+    handle_colour(colour, true, output_buffer)
 }
 
-pub fn handle_bg(colour: Colour) -> String {
-    handle_colour(colour, false)
+pub fn handle_bg(colour: Colour, output_buffer: &mut Vec<u8>) {
+    handle_colour(colour, false, output_buffer)
 }
 
-fn handle_colour(colour: Colour, fg: bool) -> String {
-    // There has to be a way to construct this without heap allocating
+fn handle_colour(colour: Colour, fg: bool, output_buffer: &mut Vec<u8>) {
     match colour {
         Colour::Normal(n) => {
-            let last = n.decode();
             if fg {
-                format!("{}{}", FG_PRE_DIGIT_NORMAL, last)
+                output_buffer.push(FG_PRE_DIGIT_NORMAL);
             } else {
-                format!("{}{}", BG_PRE_DIGIT_NORMAL, last)
+                output_buffer.push(BG_PRE_DIGIT_NORMAL);
             }
+            output_buffer.push(n.decode());
         },
         Colour::Bright(b) => {
-            let last = b.decode();
             if fg {
-                format!("{}{}", FG_PRE_DIGIT_BRIGHT, last)
+                output_buffer.push(FG_PRE_DIGIT_BRIGHT);
             } else {
-                format!("{}{}", BG_PRE_DIGIT_BRIGHT, last)
+                output_buffer.push(BG_PRE_DIGIT_BRIGHT);
             }
+            output_buffer.push(b.decode());
         },
         Colour::Extended(e) => {
             match e {
                 Extended::ColourMode(cm) => {
-                    let rgb = cm.decode();
                     if fg {
-                        format!("{};{};{}", EXTENDED_FG_BIT, COLOURMODE_SIGNAL_BIT, rgb )
+                        output_buffer.push(EXTENDED_FG_BIT);
                     } else {
-                        format!("{};{};{}", EXTENDED_BG_BIT, COLOURMODE_SIGNAL_BIT, rgb )
+                        output_buffer.push(EXTENDED_BG_BIT);
                     }
+                    output_buffer.extend_from_slice(b";");
+                    output_buffer.push(COLOURMODE_SIGNAL_BIT);
+                    output_buffer.extend_from_slice(b";");
+                    output_buffer.push(cm.decode());
                 },
                 Extended::TrueColour(tc) => {
                     let (r, g, b) = tc.decode();
                     if fg {
-                        format!("{};{};{};{};{}", EXTENDED_FG_BIT, TRUE_COLOURMODE_SIGNAL_BIT, r, g, b )
+                        output_buffer.push(EXTENDED_FG_BIT);
                     } else {
-                        format!("{};{};{};{};{}", EXTENDED_BG_BIT, TRUE_COLOURMODE_SIGNAL_BIT, r, g, b )
+                        output_buffer.push(EXTENDED_BG_BIT);
                     }
+                    output_buffer.extend_from_slice(b";");
+                    output_buffer.push(TRUE_COLOURMODE_SIGNAL_BIT);
+                    output_buffer.extend_from_slice(b";");
+                    output_buffer.push(r);
+                    output_buffer.extend_from_slice(b";");
+                    output_buffer.push(g);
+                    output_buffer.extend_from_slice(b";");
+                    output_buffer.push(b);
                 }
             }
         },
