78b3abbbb871a70cebf0f258b2452d1b12c1d83f
Xqhare <xqhare@web.de>
2026-01-27 20:29:42 +0100

    chore(input/parser): now accepts &[u8] instead of Vec<u8>

--- src/input/parser.rs
+++ src/input/parser.rs
@@ -1,9 +1,10 @@
+
 use crate::error::TalosResult;
 
 use super::Event;
 
 // TODO: Add input parser that converts the bytes to:
 // `Event::Key(Key::UP)`, `Event::Char('a')`,
-pub fn parse_byte_stream(bytes: Vec<u8>) -> TalosResult<Vec<Event>> {
+pub fn parse_byte_stream(bytes: &[u8]) -> TalosResult<Vec<Event>> {
     todo!()
 }
