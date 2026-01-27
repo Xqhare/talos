763901a131ce543ca814444e9cb797fa4a1256b1
Xqhare <xqhare@web.de>
2026-01-28 00:45:40 +0100

    refactor(input/events): Rework to use KeyEvents to handle modifiers, ctrl and such

--- src/input/event.rs
+++ src/input/event.rs
@@ -1,13 +1,23 @@
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Event {
-    Key(Key),
-    Char(char),
-    Signal(Signal),
+    KeyEvent(KeyEvent),
     Unknown(Vec<u8>), // Useful for debugging weird sequences
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct KeyEvent {
+    pub code: KeyCode,
+    pub modifiers: KeyModifiers,
+}
+
+impl KeyEvent {
+    pub fn new(code: KeyCode, modifiers: KeyModifiers) -> Self {
+        Self { code, modifiers }
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Key {
+pub enum KeyCode {
     Esc,
     Enter,
     Backspace,
@@ -23,11 +33,24 @@ pub enum Key {
     Delete,
     Insert,
     F(u8), // F1 - F12
+    Char(char),
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Signal {
-    Interrupt, // Ctrl+C
-    Quit,      // Ctrl+\
-    Suspend,   // Ctrl+Z
+pub struct KeyModifiers {
+    pub none: bool,
+    pub shift: bool,
+    pub ctrl: bool,
+    pub alt: bool,
+}
+
+impl Default for KeyModifiers {
+    fn default() -> Self {
+        Self {
+            none: true,
+            shift: false,
+            ctrl: false,
+            alt: false,
+        }
+    }
 }
