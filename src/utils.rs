6000fd6512383e1ec0b4b66e87bf7146365e72fb
Xqhare <xqhare@web.de>
2026-01-28 15:02:44 +0100

    chore(utils): make it compile

--- src/utils.rs
+++ src/utils.rs
@@ -6,11 +6,11 @@ pub fn write_all_bytes<T: Write>(writer: &mut T, bytes: &[u8]) -> TalosResult<()
     writer.write_all(bytes).map_err(Into::into)
 }
 
-pub fn u16_as_ascii_bytes(n: u16) -> &[u8] {
+pub fn u16_as_ascii_bytes(n: &u16) -> [u8; 2] {
     let mut bytes = [0u8; 2];
 
     bytes[0] = (n % 256) as u8;
     bytes[1] = (n / 256) as u8;
 
-    &bytes
+    bytes
 }
