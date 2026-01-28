3a8741ab15291c03b27689a4d84f24025ed0e0fc
Xqhare <xqhare@web.de>
2026-01-28 14:52:54 +0100

    feat(utils): add u16_as_ascii_bytes helper fn

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,16 @@
 use std::io::Write;
 
-pub fn write_all_bytes<T: Write>(writer: &mut T, bytes: &[u8]) -> std::io::Result<()> {
-    writer.write_all(bytes)
+use crate::error::TalosResult;
+
+pub fn write_all_bytes<T: Write>(writer: &mut T, bytes: &[u8]) -> TalosResult<()> {
+    writer.write_all(bytes).map_err(Into::into)
+}
+
+pub fn u16_as_ascii_bytes(n: u16) -> &[u8] {
+    let mut bytes = [0u8; 2];
+
+    bytes[0] = (n % 256) as u8;
+    bytes[1] = (n / 256) as u8;
+
+    &bytes
 }
