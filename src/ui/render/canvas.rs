5eb35e9f28e5e8f514aea3d1611aeb54eaea8901
Xqhare <xqhare@web.de>
2026-02-05 04:16:45 +0100

    add(render/canvas) last_cell helper to get the coords of the last set cell

--- src/ui/render/canvas.rs
+++ src/ui/render/canvas.rs
@@ -6,6 +6,7 @@ pub struct Canvas {
     pub buffer: Vec<CCell>,
     width: u16,
     height: u16,
+    last_set_cell: Option<(u16, u16)>,
 }
 
 impl Canvas {
@@ -15,6 +16,7 @@ impl Canvas {
             buffer,
             width,
             height,
+            last_set_cell: None
         }
     }
 
@@ -44,16 +46,28 @@ impl Canvas {
 
     /// Unsafe access for performance-critical loops (like internal renderers)
     /// Panics if out of bounds.
+    ///
+    /// Also sets the last set cell
     pub fn get_mut_ccell(&mut self, x: u16, y: u16) -> &mut CCell {
+        self.last_set_cell = Some((x, y));
         &mut self.buffer[(x + y * self.width) as usize]
     }
 
     /// Safely sets a cell. Ignores the command if coordinates are out of bounds (Clipping).
+    ///
+    /// Also sets the last set cell
     pub fn set_ccell(&mut self, x: u16, y: u16, cell: CCell) {
         if x >= self.width || y >= self.height {
             return;
         }
+        self.last_set_cell = Some((x, y));
         self.buffer[(x + y * self.width) as usize] = cell;
+    }
+
+    /// Returns the coordinate of the last set cell (x, y)
+    /// Returns None if no cell has been set
+    pub fn last_cell(&self) -> Option<(u16, u16)> {
+        self.last_set_cell
     }
 }
 
