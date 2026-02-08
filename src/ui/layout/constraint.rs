c6eac15cad3ee6082895d1b979ee712a46d9e908
Xqhare <xqhare@web.de>
2026-02-08 09:15:51 +0100

    doc(ui/layout/constraint): add all missing

--- src/ui/layout/constraint.rs
+++ src/ui/layout/constraint.rs
@@ -1,8 +1,14 @@
+/// A constraint for a layout
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Constraint {
+    /// Constrains to a specific length
     Length(u16),
+    /// Constrains to a percentage of the available space
     Percentage(u16),
+    /// Constrains to a minimum length
     Min(u16),
+    /// Constrains to a ratio of the available space
     Ratio(u32, u32),
+    /// Constrains to a maximum length
     Max(u16),
 }
