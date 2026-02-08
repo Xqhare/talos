bf2e990d7cec48b65f0ab8c3fe48ac9f979b63c8
Xqhare <xqhare@web.de>
2026-02-08 04:28:56 +0100

    make clippy happy

--- src/input/parse/mod.rs
+++ src/input/parse/mod.rs
@@ -6,9 +6,19 @@ mod xterm;
 pub use xterm::XtermParser;
 
 pub trait InputParser {
+    /// Creates a new parser
     fn new() -> Self
     where
         Self: Sized;
+    /// Parses new bytes into events
+    ///
+    /// # Arguments
+    /// * `new_bytes` - The new bytes to parse
+    /// * `output` - The output buffer to write events to
+    ///
+    /// # Errors
+    /// Returns an error if the parser failed to parse the bytes
     fn parse(&mut self, new_bytes: &[u8], output: &mut Vec<Event>) -> TalosResult<()>;
+    /// Flushes the parser
     fn flush(&mut self, output: &mut Vec<Event>);
 }
