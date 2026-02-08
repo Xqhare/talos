74e97defe3626fd0117aeed7b7ed4c32d32f9adf
Xqhare <xqhare@web.de>
2026-02-08 09:16:40 +0100

    doc(ui/layout/direction): add all missing

--- src/ui/layout/direction.rs
+++ src/ui/layout/direction.rs
@@ -1,5 +1,8 @@
+/// The direction of a layout
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Direction {
+    /// Vertical layout direction
     Vertical,
+    /// Horizontal layout direction
     Horizontal,
 }
