9dea7e3661f62d5f4b6315926d1799be76c64ffc
Xqhare <xqhare@web.de>
2026-02-08 06:49:35 +0100

    doc(widgets/mod): add all missing

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -9,12 +9,21 @@ pub use number::Number;
 
 pub mod stateful;
 
+/// Widget traits for creating custom widgets
 pub mod traits {
     use crate::{
         codex::Codex,
         layout::Rect,
         render::{Canvas, Style},
     };
+
+    /// Widget trait
+    ///
+    /// A widget is a building block for your UI
+    ///
+    /// If you want to create a custom widget, you need to implement this trait.
+    /// To learn more, take a look at the widgets included in this crate and how they implement
+    /// this trait.
     pub trait Widget {
         /// Renders the widget into the specified area
         ///
@@ -25,6 +34,7 @@ pub mod traits {
         /// * `area` - The area to render into. Only this area is available for the widget to draw
         ///   into.
         fn render(&mut self, canvas: &mut Canvas, area: Rect, codex: &Codex);
+        /// Sets the primary style of the widget
         fn style(&mut self, style: Style);
     }
 }
