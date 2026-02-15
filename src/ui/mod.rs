e8faf2d222114d17a477d984081a3252d3ce39c1
Xqhare <xqhare@web.de>
2026-02-15 22:06:12 +0100

    doc(ui): massively expand doc and examples

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,3 +1,11 @@
+//! This module contains the UI components of the Talos library.
+//!
+//! The UI system is divided into two main parts:
+//!
+//! * **Layout:** The layout system is responsible for dividing the screen into different areas, and
+//!   then placing widgets in them.
+//! * **Rendering:** The rendering system is responsible for drawing the widgets to the screen.
+
 /// Layouter
 pub mod layout;
 
