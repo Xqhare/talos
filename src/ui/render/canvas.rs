a92e846d27c15563793f963305211109845e13d4
Xqhare <xqhare@web.de>
2026-04-11 06:47:20 +0200

    Revert "make clippy happy"
    This reverts commit 545f328e10cc293a88d6a39606bd1070da8198a6.

--- src/ui/render/canvas.rs
+++ src/ui/render/canvas.rs
@@ -11,10 +11,9 @@ use super::CCell;
 /// let canvas = Canvas::new(10, 20);
 /// ```
 #[derive(Clone, Debug)]
-#[must_use]
 pub struct Canvas {
     /// A 2D array of [`CCell`](struct.CCell.html)s
-    pub(crate) buffer: Vec<CCell>,
+    pub buffer: Vec<CCell>,
     width: u16,
     height: u16,
     last_set_cell: Option<(u16, u16)>,
@@ -33,7 +32,6 @@ impl Canvas {
     ///
     /// let canvas = Canvas::new(10, 20);
     /// ```
-    #[inline]
     #[must_use]
     pub fn new(width: u16, height: u16) -> Self {
         let buffer = make_default_buffer(width, height);
@@ -54,7 +52,6 @@ impl Canvas {
     /// let canvas = Canvas::new(10, 20);
     /// assert_eq!(canvas.max_height(), 19);
     /// ```
-    #[inline]
     #[must_use]
     pub fn max_height(&self) -> u16 {
         self.height.saturating_sub(1)
@@ -69,7 +66,6 @@ impl Canvas {
     /// let canvas = Canvas::new(10, 20);
     /// assert_eq!(canvas.max_width(), 9);
     /// ```
-    #[inline]
     #[must_use]
     pub fn max_width(&self) -> u16 {
         self.width.saturating_sub(1)
@@ -84,7 +80,6 @@ impl Canvas {
     /// let canvas = Canvas::new(10, 20);
     /// assert_eq!(canvas.size_rect(), Rect::new(0, 0, 10, 20));
     /// ```
-    #[inline]
     #[must_use]
     pub fn size_rect(&self) -> Rect {
         Rect::new(0, 0, self.width, self.height)
@@ -99,7 +94,6 @@ impl Canvas {
     /// let mut canvas = Canvas::new(10, 20);
     /// canvas.clear();
     /// ```
-    #[inline]
     pub fn clear(&mut self) {
         self.buffer.fill(CCell::default());
     }
@@ -114,14 +108,12 @@ impl Canvas {
     /// let cell = canvas.get_ccell(5, 10);
     /// assert_eq!(cell, CCell::default());
     /// ```
-    #[inline]
     #[must_use]
     pub fn get_ccell(&self, x: u16, y: u16) -> CCell {
         if x >= self.width || y >= self.height {
             return CCell::default();
         }
-        let index = usize::from(x).saturating_add(usize::from(y).saturating_mul(usize::from(self.width)));
-        self.buffer[index]
+        self.buffer[(x + y * self.width) as usize]
     }
 
     /// Unsafe access for performance-critical loops (like internal renderers)
@@ -137,12 +129,9 @@ impl Canvas {
     /// let cell = canvas.get_mut_ccell(5, 10);
     /// *cell = CCell::default();
     /// ```
-    #[inline]
-    #[must_use]
     pub fn get_mut_ccell(&mut self, x: u16, y: u16) -> &mut CCell {
         self.last_set_cell = Some((x, y));
-        let index = usize::from(x).saturating_add(usize::from(y).saturating_mul(usize::from(self.width)));
-        &mut self.buffer[index]
+        &mut self.buffer[(x + y * self.width) as usize]
     }
 
     /// Safely sets a cell. Ignores the command if coordinates are out of bounds (Clipping).
@@ -156,14 +145,12 @@ impl Canvas {
     /// let mut canvas = Canvas::new(10, 20);
     /// canvas.set_ccell(5, 10, CCell::default());
     /// ```
-    #[inline]
     pub fn set_ccell(&mut self, x: u16, y: u16, cell: CCell) {
         if x >= self.width || y >= self.height {
             return;
         }
         self.last_set_cell = Some((x, y));
-        let index = usize::from(x).saturating_add(usize::from(y).saturating_mul(usize::from(self.width)));
-        self.buffer[index] = cell;
+        self.buffer[(x + y * self.width) as usize] = cell;
     }
 
     /// Returns the coordinate of the last set cell (x, y)
@@ -177,7 +164,6 @@ impl Canvas {
     /// canvas.set_ccell(5, 10, CCell::default());
     /// assert_eq!(canvas.last_cell(), Some((5, 10)));
     /// ```
-    #[inline]
     #[must_use]
     pub fn last_cell(&self) -> Option<(u16, u16)> {
         self.last_set_cell
@@ -185,5 +171,5 @@ impl Canvas {
 }
 
 fn make_default_buffer(width: u16, height: u16) -> Vec<CCell> {
-    vec![CCell::default(); usize::from(width).saturating_mul(usize::from(height))]
+    vec![CCell::default(); width as usize * height as usize]
 }
