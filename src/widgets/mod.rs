fc4a590e974f1e9599270737b2f666ede476f1d1
Xqhare <xqhare@web.de>
2026-04-11 06:47:20 +0200

    Revert "make clippy happy"
    This reverts commit 6358eca4890fd09e2d2f9ebf7672684e31dd99e5.

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -28,16 +28,15 @@
 //! }
 //! ```
 
-/// Area
-pub mod area;
-/// Block
-pub mod block;
-/// Internal Text
-pub mod internal_text;
-/// Number
-pub mod number;
-/// Text
-pub mod text;
+mod area;
+mod block;
+mod internal_text;
+mod number;
+mod text;
+pub use area::Area;
+pub use block::Block;
+pub use number::Number;
+pub use text::Text;
 
 /// Stateful widgets
 pub mod stateful;
@@ -47,8 +46,7 @@ pub mod traits {
     use crate::{
         codex::Codex,
         layout::Rect,
-        ui::render::canvas::Canvas,
-        ui::render::style::Style,
+        render::{Canvas, Style},
     };
 
     /// Widget trait
@@ -74,7 +72,6 @@ pub mod traits {
         /// Returns the widget with the specified style set
         ///
         /// This is a convenience method for chaining
-        #[inline]
         #[must_use]
         fn with_style(mut self, style: Style) -> Self
         where
@@ -86,11 +83,9 @@ pub mod traits {
     }
 
     impl Widget for &mut dyn Widget {
-        #[inline]
         fn render(&mut self, canvas: &mut Canvas, area: Rect, codex: &Codex) {
             (**self).render(canvas, area, codex);
         }
-        #[inline]
         fn style(&mut self, style: Style) {
             (**self).style(style);
         }
@@ -100,8 +95,6 @@ pub mod traits {
     ///
     /// Useful for passing a vector of widgets to a widget that expects a vector of dynamic
     /// widgets like `List` or `MenuButton`.
-    #[inline]
-    #[must_use]
     pub fn make_dyn_iter<'a, I, W>(iter: I) -> Vec<&'a mut dyn Widget>
     where
         I: Iterator<Item = &'a mut W>,
