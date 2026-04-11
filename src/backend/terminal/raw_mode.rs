951ecd8e71c7aa9398ce9b38bd5f53dbacd754a1
Xqhare <xqhare@web.de>
2026-04-11 03:29:10 +0200

    make clippy happy

--- src/backend/terminal/raw_mode.rs
+++ src/backend/terminal/raw_mode.rs
@@ -1,20 +1,16 @@
-use std::io::Write;
 use std::os::fd;
-use std::sync::Once;
-
+use crate::error::Result as TalosResult;
 use crate::backend::sys::{disable_raw_mode, enable_raw_mode};
-use crate::error::TalosResult;
-use crate::utils::constants::ansi::{EXIT_ALT_SCREEN, SHOW_CURSOR};
 
+/// A guard for raw mode
 pub struct RawMode {
     original_termios: libc::termios,
-    fd_stdin: fd::RawFd,
+    fd_stdin: i32,
 }
 
 impl RawMode {
+    /// Enables raw mode
     pub fn enable(fd_stdin: fd::RawFd) -> TalosResult<RawMode> {
-        // Install panic hook - ALWAYS CALL BEFORE `enable_rawmode`
-        install_panic_hook();
         let (original_termios, fd_stdin) = enable_raw_mode(fd_stdin)?;
         Ok(RawMode {
             original_termios,
@@ -27,19 +23,4 @@ impl Drop for RawMode {
     fn drop(&mut self) {
         disable_raw_mode(self.fd_stdin, &self.original_termios);
     }
-}
-
-static INIT: Once = Once::new();
-
-fn install_panic_hook() {
-    INIT.call_once(|| {
-        let def_hook = std::panic::take_hook();
-        std::panic::set_hook(Box::new(move |info| {
-            let mut std_err = std::io::stderr();
-            let _ = std_err.write_all(EXIT_ALT_SCREEN.as_bytes());
-            let _ = std_err.write_all(SHOW_CURSOR.as_bytes());
-            let _ = std_err.flush();
-            def_hook(info);
-        }));
-    });
 }
