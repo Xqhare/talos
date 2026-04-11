2dc6d9797402cd52ba951f52f1d083a92beb65ab
Xqhare <xqhare@web.de>
2026-04-11 03:29:13 +0200

    make clippy happy

--- src/backend/sys/unix/raw_mode.rs
+++ src/backend/sys/unix/raw_mode.rs
@@ -1,17 +1,16 @@
-use crate::error::TalosResult;
-use std::{io, mem, os::fd};
+use crate::error::Result as TalosResult;
+use std::{mem, os::fd};
 
 pub fn enable_rawmode(fd_stdin: fd::RawFd) -> TalosResult<(libc::termios, i32)> {
     unsafe {
         let mut termios = mem::zeroed();
 
         if libc::tcgetattr(fd_stdin, &raw mut termios) == -1 {
-            return Err(io::Error::last_os_error().into());
+            return Err(std::io::Error::last_os_error().into());
         }
 
         let original_termios = termios;
 
-        // Turn off software flow control, carriage return translation
         termios.c_iflag &= !(libc::IGNBRK
             | libc::BRKINT
             | libc::PARMRK
@@ -20,23 +19,16 @@ pub fn enable_rawmode(fd_stdin: fd::RawFd) -> TalosResult<(libc::termios, i32)> 
             | libc::IGNCR
             | libc::ICRNL
             | libc::IXON);
-
-        // Turn off output processing
         termios.c_oflag &= !libc::OPOST;
-
-        // Turn off echoing, canonical mode (line-by-line), and signals (Ctrl+C)
-        termios.c_lflag &= !(libc::ECHO | libc::ECHONL | libc::ICANON | libc::IEXTEN);
-
-        // Control flags: Set 8 bits per char
+        termios.c_lflag &= !(libc::ECHO | libc::ECHONL | libc::ICANON | libc::ISIG | libc::IEXTEN);
         termios.c_cflag &= !(libc::CSIZE | libc::PARENB);
         termios.c_cflag |= libc::CS8;
 
-        // Non blocking read - I hope
         termios.c_cc[libc::VMIN] = 0;
         termios.c_cc[libc::VTIME] = 0;
 
         if libc::tcsetattr(fd_stdin, libc::TCSAFLUSH, &raw const termios) == -1 {
-            return Err(io::Error::last_os_error().into());
+            return Err(std::io::Error::last_os_error().into());
         }
 
         Ok((original_termios, fd_stdin))
@@ -45,6 +37,6 @@ pub fn enable_rawmode(fd_stdin: fd::RawFd) -> TalosResult<(libc::termios, i32)> 
 
 pub fn disable_rawmode(fd_stdin: fd::RawFd, original_termios: &libc::termios) {
     unsafe {
-        libc::tcsetattr(fd_stdin, libc::TCSAFLUSH, original_termios);
+        let _ = libc::tcsetattr(fd_stdin, libc::TCSAFLUSH, &raw const *original_termios);
     }
 }
